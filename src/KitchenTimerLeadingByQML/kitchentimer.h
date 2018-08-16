a887d7993b68b68b0d642ebe3bf82685892d4892
Takayuki ORITO <iori.ayane@gmail.com>
2018-08-17 01:19:09 +0900

    タイマーアプリの基礎作成

---
+++ src/KitchenTimerLeadingByQML/kitchentimer.h
@@ -0,0 +1,52 @@
+#ifndef KITCHENTIMER_H
+#define KITCHENTIMER_H
+
+#include <QElapsedTimer>
+#include <QObject>
+#include <QTimer>
+
+class KitchenTimer : public QObject
+{
+  Q_OBJECT
+
+  Q_PROPERTY(QString remainTimeString READ remainTimeString NOTIFY remainTimeStringChanged)
+  Q_PROPERTY(int countTime READ countTime WRITE setCountTime NOTIFY countTimeChanged)
+  Q_PROPERTY(bool fired READ fired NOTIFY firedChanged)
+  Q_PROPERTY(bool running READ running WRITE setRunning NOTIFY runningChanged)
+
+public:
+  explicit KitchenTimer(QObject *parent = nullptr);
+
+  QString remainTimeString() const;
+  int countTime() const;
+  bool fired() const;
+  bool running() const;
+
+signals:
+  void remainTimeStringChanged(QString remainTimeString);
+  void countTimeChanged(int countTime);
+  void firedChanged(bool fired);
+  void runningChanged(bool running);
+
+public slots:
+  void setRemainTimeString(QString remainTimeString);
+  void setCountTime(int countTime);
+  void setFired(bool fired);
+  void setRunning(bool running);
+  void start();
+  void clear();
+  void triggered();
+
+private:
+  QTimer m_timer;
+
+  QElapsedTimer m_elapse;
+  QString m_remainTimeString;
+  int m_countTime;
+  bool m_fired;
+  bool m_running;
+
+  void updateRemainTime(int remainTime);
+};
+
+#endif // KITCHENTIMER_H
---
+++ src/KitchenTimerLeadingByQML/main.cpp
@@ -0,0 +1,20 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include "kitchentimer.h"
+
+int main(int argc, char *argv[])
+{
+  QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+  QGuiApplication app(argc, argv);
+
+  qmlRegisterType<KitchenTimer>("tech.relog.plugin.kitchentimer"
+                                   , 1, 0, "KitchenTimer");
+
+  QQmlApplicationEngine engine;
+  engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+  if (engine.rootObjects().isEmpty())
+    return -1;
+
+  return app.exec();
+}
